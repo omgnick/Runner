3865e7554f45e5207698fcf9d588d2861c32450a
Dmirty Greschik <ownageteem@gmail.com>
2014-05-30 22:17:08 +0300

    Level generator beta.

---
+++ Assets/Scripts/Engine/LevelGenerator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGenerator : CachedBehaviour {
+
+	public int MaxPrefabsNumber {get; set;}
+
+	private List<LevelPrefab> prefabs = new List<LevelPrefab>();
+	private Queue<Transform> queue = new Queue<Transform>();
+	private Transform last_created;
+
+
+
+	public void AddPrefab(LevelPrefab prefab){
+		prefabs.Add(prefab);
+	}
+
+
+
+	public void StartBuildingLevel(){
+		StartCoroutine(BuildLevel());
+	}
+
+
+
+	private IEnumerator BuildLevel(){
+
+		for(int i = queue.Count; i < MaxPrefabsNumber; i++){
+
+		}
+
+		return null;
+	}
+
+
+
+	private void CreateRandomPrefab(){
+		LevelPrefab prefab = prefabs[Random.Range(0, prefabs.Count - 1)];
+
+		if(prefab.prefab == null){
+			prefab.prefab = Resources.Load(prefab.file) as GameObject; 
+		}
+
+		GameObject obj = Instantiate(prefab.prefab) as GameObject;
+
+		if(last_created == null){
+			obj.transform.position = Vector3.zero;
+			obj.transform.rotation = Quaternion.Euler(Vector3.zero);
+			obj.transform.localScale = Vector3.one;
+		} else {
+			LevelPrefab last_prefab = last_created.GetComponent<LevelPrefab>();
+			Vector3 position = last_created.position + prefab.distance; 
+			obj.transform.position = position;
+			obj.transform.rotation = Quaternion.Euler(Vector3.zero);
+			obj.transform.localScale = Vector3.one;
+		}
+
+		last_created = obj.transform;
+	}
+}
---
+++ Assets/Scripts/Engine/LevelPrefab.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LevelPrefab : CachedBehaviour {
+
+	public string file;
+	public Vector3 distance;
+	public Vector3 moveDirection;
+	public Vector3 startPosition;
+	public GameObject prefab;
+}
